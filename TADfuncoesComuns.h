30b09d7a7167652764ce349c7e368d1ce0eb9712
lucasaugsantos <lucasaugsantos@gmail.com>
2015-05-08 01:09:04 -0300

    Update TADfuncoesComuns.h

--- TADfuncoesComuns.h
+++ TADfuncoesComuns.h
@@ -1,11 +1,45 @@
 #ifndef __TADFUNCOESCOMUNS_H_
 #define __TADFUNCOESCOMUNS_H_
-
+//
+// Input: 
+// Output: 
+// Pré-condição: 
+// Pós condição:
 long int fatorial(int n);
+
+//
+// Input: 
+// Output: 
+// Pré-condição: 
+// Pós condição:
 float custo(float** matriz, int* vetor, int n);
+
+//
+// Input: 
+// Output: 
+// Pré-condição: 
+// Pós condição:
 int* alocarVetor(int n);
+
+//
+// Input: 
+// Output: 
+// Pré-condição: 
+// Pós condição:
 float** alocarMatriz(int n);
+
+//
+// Input: 
+// Output: 
+// Pré-condição: 
+// Pós condição:
 void liberarMatriz(int n, float** matriz);
+
+//
+// Input: 
+// Output: 
+// Pré-condição: 
+// Pós condição:
 void imprimirCaminho(int* caminho, float soma, int n);
 
 
